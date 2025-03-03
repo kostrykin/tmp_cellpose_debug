34c5738859d3decd53e96544a551f00b07727ca8
Leonid Kostrykin <void@evoid.de>
2025-03-03 19:06:56 +0100

    Reduce MWE

--- cp_test.py
+++ cp_test.py
@@ -10,27 +10,19 @@ torch.manual_seed(0)
 
 gpu = False
 model_type = 'cyto2'
-channels = [2, 3]
-
 options = {
     "diameter": None,
-    "resample": True,
     "flow_threshold": 0.4,
-    "cellprob_threshold": 0.0,
-    "niter": None,
-    "do_3D": False,
-    "rescale": None,
-    "invert": False,
+    "channels": [2, 3],
 }
-
 
 img = skimage.io.imread('img02.png')
 
 model1 = models.CellposeModel(gpu=gpu, model_type=model_type)
-masks1, flows1, styles1 = model1.eval(img, channels=channels, **options)
+masks1, flows1, styles1 = model1.eval(img, **options)
 
 model2 = models.Cellpose(gpu=gpu, model_type=model_type)
-masks2, flows2, styles2, _ = model2.eval(img, channels=channels, **options)
+masks2, flows2, styles2, _ = model2.eval(img, **options)
 
 assert np.allclose(masks1, masks2), f'{masks1.sum()}, {masks2.sum()}'
 assert np.allclose(flows1, flows2)
